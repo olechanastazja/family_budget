605b82655b541bf022998f0dce8b54646782f5a2
Agnieszka Olech <agnieszka.anastazja.olech@gmail.com>
2022-09-04 13:15:04 +0200

    Fix permission class

--- family_budget/budget_core/permissions.py
+++ family_budget/budget_core/permissions.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsBudgetOwnerOrReadOnly(IsAuthenticated):
     """
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
@@ -11,6 +12,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
-            return True
+            return obj.owner == request.user or obj.shared_with.filter(id=request.user.id).exists()
         # Instance must have an attribute named `owner`.
         return obj.owner == request.user
