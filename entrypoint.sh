049278346f0ee5af266873ed28c6fc1a6799754b
Agnieszka Olech <agnieszka.anastazja.olech@gmail.com>
2022-09-05 21:42:44 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 python manage.py migrate
 python manage.py loadfixtures
 python manage.py createsuperuser --noinput
